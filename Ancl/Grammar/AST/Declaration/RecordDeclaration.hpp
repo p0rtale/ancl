2d209e37e9ac6d278b26f55e8049fc0b558be403
p0rtale <yupetryaev@mail.ru>
2024-03-30 16:37:48 +0300

    update record decl

--- Ancl/Grammar/AST/Declaration/RecordDeclaration.hpp
+++ Ancl/Grammar/AST/Declaration/RecordDeclaration.hpp
@@ -9,16 +9,33 @@ namespace ast {
 
 class RecordDeclaration: public TagDeclaration {
 public:
-    RecordDeclaration(std::vector<FieldDeclaration*> fields,
-                      bool isUnion = false)
-        : m_Fields(std::move(fields)), m_IsUnion(isUnion) {}
+    RecordDeclaration(bool isUnion = false): m_IsUnion(isUnion) {}
 
     void Accept(AstVisitor& visitor) override {
         visitor.Visit(*this);
     }
 
+    void AddInternalTagDecl(TagDeclaration* tagDecl) {
+        m_InternalDecls.push_back(tagDecl);
+    }
+
+    void AddField(FieldDeclaration* field) {
+        m_InternalDecls.push_back(field);
+    }
+
     std::vector<FieldDeclaration*> GetFields() const {
-        return m_Fields;
+        auto fields = std::vector<FieldDeclaration*>{};
+        for (const auto& decl : m_InternalDecls) {
+            auto field = dynamic_cast<FieldDeclaration*>(decl);
+            if (field) {
+                fields.push_back(field);
+            }
+        }
+        return fields;
+    }
+
+    std::vector<Declaration*> GetInternalDecls() const {
+        return m_InternalDecls;
     }
 
     bool IsStruct() const override {
@@ -34,7 +51,13 @@ public:
     }
 
 private:
-    std::vector<FieldDeclaration*> m_Fields;
+    // TODO: simplify
+    // FieldDeclaration | TagDeclaration
+    std::vector<Declaration*> m_InternalDecls;
+
+    // std::vector<FieldDeclaration*> m_Fields;
+    // std::vector<TagDeclaration*> m_InternalDecls;
+
     bool m_IsUnion = false;
 };
 
