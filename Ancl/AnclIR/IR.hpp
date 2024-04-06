c7a908058d3d40f49e655d476f8b6db5966cfe96
p0rtale <yupetryaev@mail.ru>
2024-04-06 20:18:35 +0300

    add IR to play

---
+++ Ancl/AnclIR/IR.hpp
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <Ancl/AnclIR/Value.hpp>
+#include <Ancl/AnclIR/BasicBlock.hpp>
+
+
+/*
+=====================================================================
+                              Constant
+=====================================================================
+*/
+
+#include <Ancl/AnclIR/Constant/Constant.hpp>
+#include <Ancl/AnclIR/Constant/FloatConstant.hpp>
+#include <Ancl/AnclIR/Constant/Function.hpp>
+#include <Ancl/AnclIR/Constant/GlobalValue.hpp>
+#include <Ancl/AnclIR/Constant/GlobalVariable.hpp>
+#include <Ancl/AnclIR/Constant/IntConstant.hpp>
+
+
+/*
+=====================================================================
+                             Instruction
+=====================================================================
+*/
+
+#include <Ancl/AnclIR/Instruction/AllocaInstruction.hpp>
+#include <Ancl/AnclIR/Instruction/BinaryInstruction.hpp>
+#include <Ancl/AnclIR/Instruction/BranchInstruction.hpp>
+#include <Ancl/AnclIR/Instruction/CallInstruction.hpp>
+#include <Ancl/AnclIR/Instruction/CastInstruction.hpp>
+#include <Ancl/AnclIR/Instruction/CompareInstruction.hpp>
+#include <Ancl/AnclIR/Instruction/Instruction.hpp>
+#include <Ancl/AnclIR/Instruction/LoadInstruction.hpp>
+#include <Ancl/AnclIR/Instruction/MemberInstruction.hpp>
+#include <Ancl/AnclIR/Instruction/ReturnInstruction.hpp>
+#include <Ancl/AnclIR/Instruction/StoreInstruction.hpp>
+#include <Ancl/AnclIR/Instruction/SwitchInstruction.hpp>
+#include <Ancl/AnclIR/Instruction/TerminatorInstruction.hpp>
+
+
+/*
+=====================================================================
+                             Type
+=====================================================================
+*/
+
+#include <Ancl/AnclIR/Type/ArrayType.hpp>
+#include <Ancl/AnclIR/Type/FloatType.hpp>
+#include <Ancl/AnclIR/Type/FunctionType.hpp>
+#include <Ancl/AnclIR/Type/IntType.hpp>
+#include <Ancl/AnclIR/Type/LabelType.hpp>
+#include <Ancl/AnclIR/Type/PointerType.hpp>
+#include <Ancl/AnclIR/Type/StructType.hpp>
+#include <Ancl/AnclIR/Type/Type.hpp>
+#include <Ancl/AnclIR/Type/VoidType.hpp>
---
+++ play/ir.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include <Ancl/AnclIR/IR.hpp>
+#include <Ancl/AnclIR/Program.hpp>
+
+using namespace ir;
+
+
+int main(int argc, const char** argv) {
+    return EXIT_SUCCESS;
+}
