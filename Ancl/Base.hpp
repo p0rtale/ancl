d0cf4ace090901efddb111e13aa9e4bb924185ba
p0rtale <yupetryaev@mail.ru>
2024-05-31 17:32:38 +0300

    add tuple hash

--- Ancl/Base.hpp
+++ Ancl/Base.hpp
@@ -16,3 +16,44 @@ template<typename T, typename... Args>
 constexpr TScopePtr<T> CreateScope(Args&&... args) {
     return std::make_unique<T>(std::forward<Args>(args)...);
 }
+
+namespace std {
+
+    namespace {
+
+        // Code from boost
+        // Reciprocal of the golden ratio helps spread entropy
+        //     and handles duplicates.
+        template <typename T>
+        inline void hash_combine(std::size_t& seed, const T& v) {
+            seed ^= hash<T>()(v) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+        }
+
+        template <typename Tuple, size_t Index = std::tuple_size<Tuple>::value - 1>
+        struct HashValueImpl {
+            static void apply(size_t& seed, const Tuple& tuple) {
+                HashValueImpl<Tuple, Index-1>::apply(seed, tuple);
+                hash_combine(seed, get<Index>(tuple));
+            }
+        };
+
+        template <typename Tuple>
+        struct HashValueImpl<Tuple, 0> {
+            static void apply(size_t& seed, const Tuple& tuple) {
+                hash_combine(seed, get<0>(tuple));
+            }
+        };
+
+    }
+
+    template <typename... TT>
+    struct hash<std::tuple<TT...>> {
+        size_t operator()(const std::tuple<TT...>& tt) const {                                              
+            size_t seed = 0;                             
+            HashValueImpl<std::tuple<TT...>>::apply(seed, tt);
+            return seed;
+        }
+
+    };
+
+}  // namespace std
