411598cc313fde55f23010c63cbd14d7b366d2fc
p0rtale <yupetryaev@mail.ru>
2024-06-01 09:07:35 +0300

    remove logging to the file

--- Ancl/Logger/Logger.cpp
+++ Ancl/Logger/Logger.cpp
@@ -15,9 +15,6 @@ void Logger::Init() {
     logSinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
     logSinks[0]->set_pattern("%^[%T] %n: %v%$");
 
-    logSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("logs/Ancl.log", true));
-    logSinks[1]->set_pattern("[%T] [%l] %n: %v");
-
     s_Logger = std::make_shared<spdlog::logger>("ANCL", std::begin(logSinks), std::end(logSinks));
     
     spdlog::register_logger(s_Logger);
